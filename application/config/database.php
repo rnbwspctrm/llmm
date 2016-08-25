2da5d63c80dd20aa19e20adc6e54dda066b343f0
srhLzbth <bluewhirligig@gmail.com>
2016-08-24 22:58:56 -0500

    Initial commit.

---
+++ application/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'default-connection' => 'concrete',
+    'connections' => array(
+        'concrete' => array(
+            'driver' => 'c5_pdo_mysql',
+            'server' => 'localhost',
+            'database' => 'llmm_main',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
+    ),
+);
