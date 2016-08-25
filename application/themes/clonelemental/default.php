58324c1d542f4256928bf4bc8f56b84972e35432
srhLzbth <bluewhirligig@gmail.com>
2016-08-25 00:31:15 -0500

    Added background image and basic default styles for site.

--- application/themes/clonelemental/default.php
+++ application/themes/clonelemental/default.php
@@ -3,17 +3,26 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
 <main>
-<?php
+    <div class="container">
+        <div class="col-md-8 col-md-push-2 col-content">
+            <?php
+            $a = new Area('Main');
+            $a->setAreaGridMaximumColumns(12);
+            $a->display($c);
+            ?>
+        </div>
+    </div>
+<!-- <?php
 $a = new Area('Main');
 $a->enableGridContainer();
 $a->display($c);
-?>
-
+?> -->
+<!-- 
 <?php
 $a = new Area('Page Footer');
 $a->enableGridContainer();
 $a->display($c);
-?>
+?> -->
 
 </main>
 
